2716b9469c071a8feb192cd1fca1a67ba8b26311
enver0173 <--list>
2020-02-14 09:05:02 -0600

    222

--- instansce.tf
+++ instansce.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "deployer" {
   ami           = "ami-0a887e401f7654935" 
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
-  key_name = aws_instance.deployer.key_name
+  key_name = aws_key_pair.deployer.key_name
 }
 
 
