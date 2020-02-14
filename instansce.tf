96774a3accbf9d3e7bd9af3dfa2b4509be555eff
enver0173 <--list>
2020-02-14 08:50:41 -0600

    vpc

--- instansce.tf
+++ instansce.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "enver-iaac" { 
-  ami           = "ami-0006597a195fe00ca" 
+  ami           = "ami-0a887e401f7654935" 
   instance_type = "t2.micro"
 }
 
 
 resource "aws_subnet" "my_subnet" {
-  vpc_id            = "vpc-057e6b63"
-  cidr_block        = "172.31.0.0/20"
-  availability_zone = "eu-west-1c"
+  vpc_id            = "vpc-0a24d75c86852fc9e"
+  cidr_block        = "172.31.0.0/16"
+  availability_zone = "us-east-1c"
 }
