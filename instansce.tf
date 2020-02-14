4bd74f54c82bed834a90685a4ce61714442bdefb
enver0173 <--list>
2020-02-14 08:26:15 -0600

    aded

--- instansce.tf
+++ instansce.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "enver-iaac" { 
   ami           = "ami-099a8245f5daa82bf" 
-  instance_type = "t2.micro" 
+  instance_type = "t2.micro"
+  availability_zone    = "eu-west-1b" 
  
 tags = { 
     Name = "HelloWorld" 
