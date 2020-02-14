ead9c9bb2e797ee4b25c14c16865985728dccd2b
enver0173 <--list>
2020-02-14 08:31:43 -0600

    added vpc

--- instansce.tf
+++ instansce.tf
@@ -1,7 +1,10 @@
 resource "aws_instance" "enver-iaac" { 
   ami           = "ami-099a8245f5daa82bf" 
   instance_type = "t2.micro"
-  availability_zone    = "eu-west-1b" 
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "${vpc-057e6b63}"
+  cidr_block        = "172.31.0.0/24"
+  availability_zone = "eu-west-1a"
  
 tags = { 
     Name = "HelloWorld" 
